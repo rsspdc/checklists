f222ca91fd1bac441c5bb814c5ebc0049ab04942
Richard J. Acton <richardjacton@richardjacton.net>
2026-04-23 14:05:39 +0100

    removed cc images from git forge issue template markdown files in script

--- R/yaml2md.R
+++ R/yaml2md.R
@@ -269,8 +269,10 @@ yaml2md <- function(
 		' is licensed under ',
 		'<a href="http://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">',
 		'CC BY 4.0',
-		'<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1">',
-		'<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1">',
+		ifelse(is.null(git_forge), paste0(
+			'<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1">\n',
+			'<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1">',
+		),""),
 		'</a>',
 		'</p>',
 		sep = "", file = output_file, append = TRUE
